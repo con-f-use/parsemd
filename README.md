2a2d7a07941b6b2a84f3e01def7c04e10f20cc58
con-f-use <con-f-use@gmx.net>
2016-03-19 01:57:24 +0100

    Support for Markdown meta-data; html and css cleand up; added github link

--- README.md
+++ README.md
@@ -9,9 +9,10 @@ See it in [action](http://unethische.org/misc/parsemd/parsemd.php?file=README.md
 
 Open `parsemd.php` in your browser. The markdown-file is specified with the GET variable `file`, e.g.
 
-    https://www.my-domain.com/parsemd.php?file=README.md&linenums
+    https://www.my-domain.com/parsemd.php?file=README.md&skin=sons-of-obsidian&linenums
 
 The `linenums` argument (at the end of the url) activates linenumbers for code blocks.
+The `skin` argument specifies the code hightlighting skin to use with code-prettify.
 
 Here is an example of code, that `parsemd` prettifies automatically:
 
@@ -32,6 +33,22 @@ Here is an example of code, that `parsemd` prettifies automatically:
 
 It honors `<?prettify ?>` commands (see prettify's [Getting Started](https://github.com/google/code-prettify/blob/master/docs/getting_started.md#language-hints)).
 
+Markdown meta-data can be included at the very top of the markdown file.
+An example would looke like this:
+
+    ####################################################
+    Title:  Readme for parsemd
+    Date:   Sat, Mar 19 - 01:53 PM
+    Author: con-f-use
+    E-Mail: con-f-use (at) gmx (dot) net
+    Description: This is some text describing the file,
+        it continues here and goes on and on and on and
+        further on. The indent for contionuation must
+        be at least 4 spaces. Keywords can only contain
+        letters, numbers, a dash (-) or an underscore
+        (_).
+    ####################################################
+
 ### Updating
 
 You can update the script with a simple `git pull` when in the cloning directory. If you're feeling cutting-edge also do a `git submodule update --remote`.
@@ -42,27 +59,34 @@ This scripts needs [php](https://secure.php.net), a web-browser, a web-server an
 
 ### PHP Installation (Ubuntu)
 
-To install a webserver for testing purposes use:
+To install a webserver on your local machine (for testing purposes), use:
 
 <?prettify lang=bsh?>
 
     sudo apt-get install -y lamp-server^
 
-Mind the `^`.[^2] Choose a strong MySQL password. Settings are stored in `/etc/php5/apache2/php.ini`.
+Mind the `^`[^2] and choose a strong MySQL password, when asked.
+Access your shiny new local webserver from [within your browser](http://localhost).
+If all went well, you should see the "Apache2 Ubuntu Default Page"
+Files on the server are normally only visible to the local network.
 
-After installation, served files are stored in `/var/www/html`.
+After installation, `/var/www/html` contains all files served out by your webserver.
+Settings are stored in `/etc/php5/apache2/php.ini`, if you change them you might have to run `sudo service apache2 restart`.
 For security reasons all files in there should belong to the user `www-data`.
-To make sure they do, that your normal user has write permissions and that new files are created with the right permissions run:
+To make sure they do, that your normal user has access and that new files are created with the right permissions run:
 
 <?prettify lang=bsh?>
 
-    sudo chgrp -R www-data /var/www
-    sudo chmod -R g+rwX /var/www
-    sudo usermod -a -G www-data $USER
-    find /var/www -type d -print0 | sudo xargs -0 chmod g+s
+    sudo chgrp -R www-data /var/www                           # set group
+    sudo chmod -R g+rwX /var/www                              # give group read+write
+    sudo usermod -a -G www-data $USER                         # add yourself
+    find /var/www -type d -print0 | sudo xargs -0 chmod g+s   # inherit groups within dir
 
-php-files you tested on your local webserver can later be uploaded to `astro-staff.uibk.ac.at`.
+You will have to logout completely or restart your computer before you have permissions in `/var/www`.
+php-files you tested on your local webserver can later be uploaded to to a remote webserver.
+
+Configure your router to forward port `80` in order for outside users to see your webserver via your IP.[^3]
 
 [^1]: Markdown Extra is a super-set of git-flavored [markdown](https://en.wikipedia.org/wiki/Markdown).
-
 [^2]: The circonfles `^` effectivly invokes `tasksel` from within `apt-get`. `tasksel` is a sort of meta-package or meta-task application grouping packages together.
+[^3]: Find out your: [web IP](http://www.getip.com/) address `dig +short myip.opendns.com @resolver1.opendns.com` and configure your router for [port-forwarding](http://www.wikihow.com/Set-Up-Port-Forwarding-on-a-Router) if you want your webserver to accessible over the internet, e.g. with `http://138.232.146.60/myfile.php`.
--- parsemd.php
+++ parsemd.php
@@ -1,9 +1,77 @@
 <?php
     error_reporting(-1);
     ini_set('display_errors', 'On');
-?>
+    date_default_timezone_set("UTC");
 
-<html><head><title>Parse Markdown</title>
+    $mdfl = urldecode($_GET["file"]);
+
+    $metare    = '~^[ ]{0,3}([A-Za-z0-9_-]+):\s*(.*?)$~';
+    $metamore  = '~^[ ]{4,}(.*?)$~';
+    $metastart = '~^[\-#=_/\.]{3,}(\s.*?)$~';
+    $delim     = '';
+    $meta      = array();
+    $text      = '';
+
+    try {
+        $meta['author'] = posix_getpwuid( fileowner($mdfl) )['name'];
+    } catch (Exception $e) {
+        $meta['author'] = '';
+    }
+    try {
+        $meta['date'] = date("r", filemtime($mdfl));
+    } catch (Exception $e) {
+        $meta['date'] = '';
+    }
+
+    if( !file_exists($mdfl) || !is_readable($mdfl)) die("Error reading '$mdfl'.");
+    $fh = fopen($mdfl, "r");
+
+    if(!feof($fh)) {
+        $line = fgets($fh);
+        if( preg_match($metastart, $line, $fst) ) $delim = $fst[0];
+    }
+
+    while ( !feof($fh) ) {
+        if( $delim === '' && !isset($otherline) ) {
+            $otherline = true;
+        } else {
+            $line = fgets($fh);
+        }
+        if( preg_match($metare, $line, $match) ) {
+            $key = strtolower( $match[1] );
+            $meta[$key] = $match[2];
+        }
+        elseif( isset($key) && preg_match($metamore, $line, $match) ) {
+            $meta[$key] .= $match[1];
+        }
+        elseif( preg_match($metastart, $line) || !trim($line) ) {
+            break;
+        }
+        else {
+            $text .= $line;
+            break;
+        }
+    }
+
+    if( !isset($key) ) $text .= $delim;
+    while( !feof($fh)) $text .= fgets($fh);
+
+    fclose($fh);
+
+?>
+<!DOCTYPE html>
+<html><head>
+    <title><?php echo isset($meta['title']) ? $meta['title'] : htmlspecialchars($mdfl); ?></title>
+
+    <meta charset="UTF-8">
+    <meta class="anchor" name="author"  content="<?php echo $meta['author'];?>">
+    <meta class="anchor" name="date"    content="<?php echo $meta['date']; ?>">
+    <?php
+        if(isset($meta['e-mail'])) echo
+            '<meta class="anchor" name="contact" content="'.$meta['e-mail'].'">'
+        ;
+    ?>
+
     <script type="text/x-mathjax-config">
       MathJax.Hub.Config({
         tex2jax: {
@@ -14,7 +82,7 @@
       });
     </script>
     <script type="text/javascript" async src="https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-MML-AM_CHTML"></script>
-    <script src="https://google-code-prettify.googlecode.com/svn/loader/run_prettify.js?skin=sunburst"></script><!-- sunburst, doxy, desert, sons-of-obsidian -->
+    <script src="https://google-code-prettify.googlecode.com/svn/loader/run_prettify.js?skin=<?php echo isset($_GET['skin']) ? $_GET['skin'] : "sunburst"; ?>"></script><!-- sunburst, doxy, desert, sons-of-obsidian -->
     <style type="text/css">
 
     a {
@@ -32,108 +100,47 @@
         color: #fff;
     }
 
-    button {
-        background: #357;
-        border: none;
-        color: #fff;
-        cursor: pointer;
-        line-height: 24px;
-        text-align: left;
-        padding: 20px;
-        width: 100%;
-    }
-
-    textarea {
-        -moz-box-sizing: border-box;
-        -webkit-box-sizing: border-box;
-        background: #333;
-        border: none;
-        box-sizing: border-box;
-        color: #fff;
-        font-family: monospace;
-        font-size: 16px;
-        height: 100%;
-        line-height: 28px;
-        margin: 0;
-        padding: 20px;
-        resize: none;
-        vertical-align: top;
-        width: 100%;
-    }
-
-    pre.prettyprint {
-    }
-    pre.prettyprint, code.prettyprint {
-    }
-
-    div.meta {
-        -moz-box-sizing: border-box;
-        -webkit-box-sizing: border-box;
-        -webkit-font-smoothing: antialiased;
-        background: #111;
-        box-sizing: border-box;
-        bottom: 190px;
-        left: 30%;
-        padding: 20px;
-        position: absolute;
-        width: 35%;
-    }
-
-    div.td {
-        -moz-box-sizing: border-box;
-        -webkit-box-sizing: border-box;
-        background: #eee;
-        box-sizing: border-box;
-        color: #222;
-        float: left;
-        padding: 20px;
-        width: 35%;;
-    }
-
     div.output {
         -moz-box-sizing: border-box;
         -webkit-box-sizing: border-box;
         -webkit-font-smoothing: antialiased;
         font-family: sans-serif;
         text-align: justify;
-        //bottom: 254px;
-        //box-sizing: border-box;
-        //overflow: scroll;
-        //padding: 20px;
-        // position: absolute;
-        //top: 64px;
         width: 35%;
         min-width: 600px;
         margin: auto;
     }
 
+    .output h1, h2, h3, h4, h5, h6 {
+        margin-bottom: 0;
+        margin-top: 1em;
+    }
+    .output p, ul, ol, dl {
+        margin-top: 0;
+        margin-bottom: 0.75em;
+    }
     .output a {
         color: #7bd;
     }
-
     .output a code {
         color: #7bd;
     }
-
     .output blockquote {
         border-left: 3px solid #999;
         color: #999;
     }
-
     .output code {
         background: #444;
         border: none;
         border-radius: 2px;
         color: #fff;
     }
-
     .output div.footnotes {
         font-size: 15px;
     }
-
     .output pre {
-        font-family: monospace !important;
-        text-align: left !important;
+        font-family: monospace;
+        text-align: left;
         background: #444;
         padding-left: 2em;
         border-radius: 3px;
@@ -141,13 +148,11 @@
         color: #fff;
         overflow: auto;
     }
-
     .output table {
         border-width: 1px 0 0 1px;
         border-color: #444;
         border-style: solid;
     }
-
     .output table td, .output table th {
         border-width: 0 1px 1px 0;
         border-color: #444;
@@ -155,56 +160,45 @@
         padding: 10px;
     }
     .output li {
-        margin-top: 0.1em;
-        margin-bottom: 0.1em;
+        margin-bottom: 0.2em;
     }
 
     .footnotes {
         font-style: italic;
         font-size: 75% !important;
     }
+    .footnotes li {
+        margin-bottom: -0.5em;
+    }
 
+    #footer {
+        font-size: 70%;
+    }
+    #leftfoot {
+        color: #444 !important;
+        margin-right: auto;
+        float: left;
+    }
+    #leftfoot a {
+        color: #256;
+    }
+    #rightfoot {
+        float: right;
+    }
 
-//    pre .str, code .str { color: #65B042; } /* string  - green */
-//    pre .kwd, code .kwd { color: #E28964; } /* keyword - dark pink */
-//    pre .com, code .com { color: #AEAEAE; font-style: italic; } /* comment - gray */
-//    pre .typ, code .typ { color: #89bdff; } /* type - light blue */
-//    pre .lit, code .lit { color: #3387CC; } /* literal - blue */
-//    pre .pun, code .pun { color: #fff; } /* punctuation - white */
-//    pre .pln, code .pln { color: #fff; } /* plaintext - white */
-//    pre .tag, code .tag { color: #89bdff; } /* html/xml tag    - light blue */
-//    pre .atn, code .atn { color: #bdb76b; } /* html/xml attribute name  - khaki */
-//    pre .atv, code .atv { color: #65B042; } /* html/xml attribute value - green */
-//    pre .dec, code .dec { color: #3387CC; } /* decimal - blue */
-
-//    ol.linenums { margin-top: 0; margin-bottom: 0; color: #AEAEAE; }
     li.L0, li.L1, li.L2, li.L3, li.L5, li.L6, li.L7, li.L8 {
         list-style-type: decimal !important;
     }
 
-//    @media print {
-//        pre .str, code .str { color: #060; }
-//        pre .kwd, code .kwd { color: #006; font-weight: bold; }
-//        pre .com, code .com { color: #600; font-style: italic; }
-//        pre .typ, code .typ { color: #404; font-weight: bold; }
-//        pre .lit, code .lit { color: #044; }
-//        pre .pun, code .pun { color: #440; }
-//        pre .pln, code .pln { color: #000; }
-//        pre .tag, code .tag { color: #006; font-weight: bold; }
-//        pre .atn, code .atn { color: #404; }
-//        pre .atv, code .atv { color: #060; }
-//    }
 </style>
-</head><body> <!--onload="prettyPrint()"-->
+</head><body>
 
 <div class="output">
 <?php
-    require_once "include/parsedown/Parsedown.php";
-    require_once "include/parsedown-extra/ParsedownExtra.php";
+    require_once "libs/parsedown/Parsedown.php";
+    require_once "libs/parsedown-extra/ParsedownExtra.php";
     $parsedown = new ParsedownExtra();
 
-    $mdfl = urldecode($_GET["file"]);
-    $text = file_get_contents($mdfl);
     $text = $parsedown->text($text);
     $ln = isset($_GET['linenums']) ? ' linenums=true' : '';
     $text = str_replace('<pre>', '<pre class="prettyprint'.$ln.'">', $text); // linenums
@@ -213,6 +207,21 @@
     echo $text;
     echo ''
 ?>
-<p style="font-size: 70%; text-align: right;">Back to <a href="./">main page</a>.</p>
+
+<div id="footer">
+    <span id="leftfoot">
+        Markdown rendered with
+        <a href="https://github.com/con-f-use/parsemd">
+            parsemd
+            <img src="https://raw.githubusercontent.com/github-archive/media/master/octocats/blacktocat-16.png" style="height: 1em;" alt="github-logo">
+        </a>
+    </span>
+    <span id="rightfoot">
+        Back to <a href="./">main page</a>
+    </span>
+    <div style="clear: both;"></div>
+</div>
+
+</div>
 
 </body></html>
